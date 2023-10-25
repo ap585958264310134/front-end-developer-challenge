0bbfae0d59dfce0be375d7e1716c88b397e5f7cf
ap585958264310134 <ap585958264310134@ap585958264310134.com>
2023-10-25 22:17:42 +0200

    POC IconButton component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import IconButton from './components/IconButton/IconButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <IconButton position={{
+          x: 0,
+          y: 1
+        }}/>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './IconButton.css';
+
+const ICON_SIZE = 50;
+
+interface IconButtonProps {
+  position: {
+    x: number;
+    y: number;
+  }
+}
+
+export default function IconButton(props: IconButtonProps) {
+  const style = {
+    "--icon-button-size": `${ ICON_SIZE }px`,
+    backgroundPositionX: props.position.x * ICON_SIZE * -1,
+    backgroundPositionY: props.position.y * ICON_SIZE * -1
+  } as React.CSSProperties;
+
+  return (
+    <div className="icon-button" style={style}>
+      
+    </div>
+  );
+}
+
